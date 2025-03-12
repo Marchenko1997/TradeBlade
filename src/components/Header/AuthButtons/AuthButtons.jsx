b6c06b8f9a295a0f843567d94794be556d7999ae
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-12 09:15:42 +0100

    updating header buttons

--- src/components/Header/AuthButtons/AuthButtons.jsx
+++ src/components/Header/AuthButtons/AuthButtons.jsx
@@ -1,12 +1,37 @@
-import { ButtonContainer, RegisterButton, EnterButton } from "./AuthButtons.styled"
+import { useState, useEffect } from "react";
+import {
+  ButtonContainer,
+  RegisterButton,
+  EnterButton,
+} from "./AuthButtons.styled";
 
 const AuthButtons = () => {
-  return (
-      <ButtonContainer>
-          <RegisterButton>РЕГИСТРАЦИЯ</RegisterButton>
-          <EnterButton>ВОЙТИ</EnterButton>
-    </ButtonContainer>
-  )
-}
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
 
-export default AuthButtons
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <ButtonContainer>
+      {isDesktop ? (
+        <>
+          <EnterButton>ВХОД</EnterButton>
+          <RegisterButton>РЕГИСТРАЦИЯ</RegisterButton>
+        </>
+      ) : (
+        <>
+          <RegisterButton>РЕГИСТРАЦИЯ</RegisterButton>
+          <EnterButton>Войти</EnterButton>
+        </>
+      )}
+    </ButtonContainer>
+  );
+};
+
+export default AuthButtons;
--- src/components/Header/AuthButtons/AuthButtons.styled.jsx
+++ src/components/Header/AuthButtons/AuthButtons.styled.jsx
@@ -6,6 +6,7 @@ export const ButtonContainer = styled.div`
 
     @media (min-width: 768px) {
       gap: 12px;
+      
     }
 `
 export const RegisterButton = styled.button`
@@ -44,13 +45,20 @@ export const EnterButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 16px, 24px;
-  color: rgb(3, 7, 24);
-  background-color: var(--primary-cyan);
+  color: var(--primary-cyan);
+  background-color: var(--primary-purple);
+  border: 2px solid rgb(87, 225, 255);
   border-radius: 3px;
   font-size: 16px;
   font-weight: 500;
   line-height: 1.25;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: var(--primary-purple);
+    color: var(--primary-cyan);
+    border: 2px solid rgb(87, 225, 255);
+  }
 
   @media (min-width: 768px) {
     font-size: 14px;
@@ -59,11 +67,8 @@ export const EnterButton = styled.button`
     line-height: 1.3;
     width: 86px;
     height: 42px;
-  }
-
-  &:hover {
-    background-color: var(--primary-purple);
-    color: var(--primary-cyan);
+    background-color: rgba(87, 225, 255, 0.2);
     border: 2px solid rgb(87, 225, 255);
+    color: var(--primary-cyan);
   }
 `;
